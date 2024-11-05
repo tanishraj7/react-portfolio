0a527b8c1c5f099edf16049f8b08a7b4c419aa37
Tanish Raj Singh <tanishrajsingh2604@gmail.com>
2024-11-06 03:38:49 +0530

    slot2

--- tanish-raj-singh/src/hero/Hero.jsx
+++ tanish-raj-singh/src/hero/Hero.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import styles from './HeroStyles.module.css'
-import themeIcon from '../assets/sun.svg'
-import githubIcon from '../assets/github-light.svg'
-import linkedinIcon from '../assets/linkedin-light.svg'
+import sun from '../assets/sun.svg'
+import githubLight from '../assets/github-light.svg'
+import githubDark from '../assets/github-dark.svg'
+import linkedinDark from '../assets/linkedin-dark.svg'
+import linkedinLight from '../assets/linkedin-light.svg'
+import moon from '../assets/moon.svg'
 import CV from '../assets/tanish-raj-cv.pdf'
+import { useTheme } from '../common/ThemeContext'
 
 function Hero() {
-  return (
+    const {theme, toggleTheme}= useTheme();
+    const themeIcon= theme === 'light'? sun: moon;
+    const linkedinIcon= theme === 'light'? linkedinLight: linkedinDark;
+    const githubIcon= theme === 'light'? githubLight: githubDark;
+
+    return (
     <section id='hero' className={styles.container}>
         <div className={styles.colorModeContainer}>
             <img src='https://media.licdn.com/dms/image/v2/D5603AQHpVvrQGIbXZw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1729991619033?e=1736380800&v=beta&t=lPb98_lUhNgu-0yCvsAjyn-ReCE9kHy622YQQFn_5h4' alt='profile pic' className={styles.hero}></img>
-            <img src={themeIcon} alt='theme icon' className={styles.colorMode}></img>
+            <img src={themeIcon} alt='theme icon' className={styles.colorMode} onClick={toggleTheme}></img>
         </div>
         <div className={styles.info}>
             <h1>Tanish Raj <br/> Singh</h1>
@@ -23,7 +32,6 @@ function Hero() {
                 <img src={githubIcon} alt='github'></img>
                 </a>
             </span>
-            <p>its all in the mind!!</p>
             <a href={CV} download>
                 <button className='hover'>Resume</button>
             </a>
