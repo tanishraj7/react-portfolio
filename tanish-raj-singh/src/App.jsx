dd34a05bd61f27d5c27e0fc8bfc932479a32a4a8
Tanish Raj Singh <tanishrajsingh2604@gmail.com>
2024-11-06 18:27:06 +0530

    slot 3

--- tanish-raj-singh/src/App.jsx
+++ tanish-raj-singh/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Hero from './hero/Hero'
+import Projects from './projects/Projects'
 
 function App() {
 
   return (
     <>
     <Hero/>
+    <Projects/>
     </>
   )
 }
