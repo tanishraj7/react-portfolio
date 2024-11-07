6e75d0022abb85a0f75412cafe9c6a9b9ddb04c5
Tanish Raj Singh <tanishrajsingh2604@gmail.com>
2024-11-07 12:44:41 +0530

    slot 4

--- tanish-raj-singh/src/App.jsx
+++ tanish-raj-singh/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './hero/Hero'
 import Projects from './projects/Projects'
+import Skills from './skills/Skills'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Hero/>
     <Projects/>
+    <Skills/>
     </>
   )
 }
